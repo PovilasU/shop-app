0ab0cf9fdb770f1649c3dcc57ca76646cd64d194
PovilasU <povilas.urbonas85@gmail.com>
2025-07-09 21:33:01 +0300

    feat: add fetchProducts function using raw fetch with GraphQL query

---
+++ src/api/fetchProducts.ts
@@ -0,0 +1,38 @@
+export const fetchProducts = async () => {
+  const query = `
+    {
+      products(first: 20) {
+        edges {
+          node {
+            id
+            title
+            description
+            featuredImage {
+              id
+              url
+            }
+            variants(first: 3) {
+              edges {
+                node {
+                  price {
+                    amount
+                    currencyCode
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const res = await fetch('https://mock.shop/api', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query }),
+  });
+
+  const json = await res.json();
+  return json.data.products.edges.map((edge: any) => edge.node);
+};
