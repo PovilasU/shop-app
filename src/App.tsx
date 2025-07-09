8f02e659aff5c3ba0eecf06c0d512f3934a571a9
PovilasU <povilas.urbonas85@gmail.com>
2025-07-09 21:17:47 +0300

    Initial commit with tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-xl shadow-md text-center">
+        <h1 className="text-3xl font-bold text-blue-600 mb-4">
+          🛍️ Welcome to Shop-App
+        </h1>
+        <p className="text-gray-700">Tailwind CSS is working!</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
