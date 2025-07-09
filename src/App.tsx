5fd99f7aa984f4c5b73c5f52720e0b11df9aec7c
PovilasU <povilas.urbonas85@gmail.com>
2025-07-09 21:44:02 +0300

    feat: render ProductGrid in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-
-import './App.css'
+import ProductGrid from "./components/ProductGrid";
 
 function App() {
-
   return (
-     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-xl shadow-md text-center">
-        <h1 className="text-3xl font-bold text-blue-600 mb-4">
-          🛍️ Welcome to Shop-App
-        </h1>
-        <p className="text-gray-700">Tailwind CSS is working!</p>
-      </div>
-    </div>
-  )
+    <main className="min-h-screen bg-gray-50">
+      <ProductGrid />
+    </main>
+  );
 }
 
-export default App
+export default App;
